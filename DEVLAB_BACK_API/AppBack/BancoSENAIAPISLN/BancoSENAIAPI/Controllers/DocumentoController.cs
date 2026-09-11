f15f90cb4e759d5c7bf12555c3e6efb4e1c0f3e6
davidvsantos10-prog <david.v.santos10@aluno.senai.br>
2026-09-11 08:49:21 -0300

    feat(documentos): inicio uploud endpoint

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -9,7 +9,13 @@ namespace BancoSENAIAPI.Controllers
         private readonly string _caminhoRaiz = Path.Combine(Directory.GetCurrentDirectory(), "ClienteArquivos");
 
     };
-    private static List<Models.DocumentoMetadado> _documentosMetadados = new List<Models.DocumentoMetadado>{};
+    private static List<Models.DocumentoMetadado> _documentosMetadados = new List<Models.DocumentoMetadado> { };
 
     private static int _nextId = 1;
+
+    [HttpPost("uploud/{codigoCliente}")]
+    public async Task<ActionResult> AnexarArquivo(int codigoCliente, IFormFile arquivo)
+    {
+
+    }
 }
