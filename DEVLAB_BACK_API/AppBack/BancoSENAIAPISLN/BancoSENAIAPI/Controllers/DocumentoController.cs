c47b71f8f4e421e7db78393c1f8d1fbe92621db3
davidvsantos10-prog <david.v.santos10@aluno.senai.br>
2026-09-11 09:16:28 -0300

    feat(documentos): alguma coisa 2

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BancoSENAIAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DocumentoController : Controller
+    {
+        private readonly string _caminhoRaiz = Path.Combine
+            (Directory.GetCurrentDirectory(),
+            "ClienteArquivos");
+
+    };
+    private static List<Models.DocumentoMetadado> _documentosMetadados = new List<Models.DocumentoMetadado> { };
+
+    private static int _nextId = 1;
+
+    [HttpPost("uploud/{codigoCliente}")]
+    public async Task<IActionResult> AnexarArquivo(int codigoCliente, IFormFile arquivo)
+    {
+        if (arquivo == null || arquivo.Length == 0)
+        {
+            return BadRequest("Nenhum arquivo foi criado.");
+        }
+
+        string.pastaCliente = Path.Combine(_caminhoRaiz, codigoCliente.ToString());
+
+        if (!Directory.Exists(pastaCliente))
+        {
+            Directory.CreateDirectory(pastaCliente);
+        }
+        string extensao = Path.GetExtension(arquivo.FileName);
+        string nomeOriginal = Path.GetFileNameWithoutExtension(arquivo.FileName);
+        string NovoNome = $"{codigoCliente}_{nomeOriginal}_{Guid.NewGuid()}{extensao}";
+        string caminhoFinal = HttpPatchAttribute.Combine(pastaCliente, novoNome);
+
+        using (var stream = new FileStream(caminhoFinal, FileMode.Create))
+        {
+
+            await arquivo.CopyToAsync(stream);
+
+
+        }
+    }
+
+}
