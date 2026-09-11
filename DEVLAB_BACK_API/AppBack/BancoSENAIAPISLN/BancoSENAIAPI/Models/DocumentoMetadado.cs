d77ecff3d19a25b7d751f5a0539c204ace3c5b4b
davidvsantos10-prog <david.v.santos10@aluno.senai.br>
2026-09-11 08:26:57 -0300

    feat(documentos/:criacaodocontrollermodeldedocumentos

---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadado.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace BancoSENAIAPI.Models
+{
+    public class DocumentoMetadado
+    {
+        public int Id { get; set; }
+        public string Extensao { get; set; }
+        public string Caminho { get; set; }
+        public string CodigoCliente { get; set; }
+    }
+}
