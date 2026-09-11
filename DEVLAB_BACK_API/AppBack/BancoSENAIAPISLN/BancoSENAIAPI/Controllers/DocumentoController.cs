ecbcbacac72db5973e5b0a41cd14003aa82988ef
davidvsantos10-prog <david.v.santos10@aluno.senai.br>
2026-09-11 09:02:13 -0300

    feature(documentos): alguma coisa

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BancoSENAIAPI.Controllers
 {
@@ -6,7 +7,9 @@ namespace BancoSENAIAPI.Controllers
     [Route("api/v1/[controller]")]
     public class DocumentoController : Controller
     {
-        private readonly string _caminhoRaiz = Path.Combine(Directory.GetCurrentDirectory(), "ClienteArquivos");
+        private readonly string _caminhoRaiz = Path.Combine
+            (Directory.GetCurrentDirectory(),
+            "ClienteArquivos");
 
     };
     private static List<Models.DocumentoMetadado> _documentosMetadados = new List<Models.DocumentoMetadado> { };
@@ -14,8 +17,19 @@ namespace BancoSENAIAPI.Controllers
     private static int _nextId = 1;
 
     [HttpPost("uploud/{codigoCliente}")]
-    public async Task<ActionResult> AnexarArquivo(int codigoCliente, IFormFile arquivo)
+    public async Task<IActionResult> AnexarArquivo(int codigoCliente, IFormFile arquivo)
     {
+        if (arquivo == null || arquivo.Length == 0)
+        {
+            return BadRequest("Nenhum arquivo foi criado.");
+        }
 
+        string.pastaCliente = Path.Combine(_caminhoRaiz, codigoCliente.ToString());
+
+        if (!Directory.Exists(pastaCliente))
+        {
+            Directory.CreateDirectory(pastaCliente);
+        }
     }
+
 }
